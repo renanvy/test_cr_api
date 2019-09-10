114be7d2ad9a5ba4dbd6f9e069d6a67767d4dbd0
Renan Carneiro <renanvy@gmail.com>
2019-09-10 14:17:29 -0300

    Add validations to CartItem model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ApplicationRecord
+  validates :quantity, 
+    presence: true, 
+    numericality: { only_integer: true, greater_than: 0 }
+
   belongs_to :cart
   belongs_to :medicine
 end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :quantity }
+    it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+  end
+
   context 'relationships' do
     it { is_expected.to belong_to :cart }
     it { is_expected.to belong_to :medicine }
