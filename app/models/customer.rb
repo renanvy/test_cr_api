1ba2f60c2b2997083d320448f9d1cb19d84a7fdb
Renan Carneiro <renanvy@gmail.com>
2019-09-10 15:13:50 -0300

    Returns customer name formatted

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   validates :name, presence: true
 
   has_many :carts
+
+  def name
+    self[:name].try(:titleize)
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Customer, type: :model do
   context 'relationships' do
     it { is_expected.to have_many :carts }
   end
+
+  context '#name' do
+    let(:customer) { Customer.new(name: 'acnase gel') }
+
+    it 'returns name titleized' do
+      expect(customer.name).to eq('Acnase Gel')
+    end
+  end
 end
