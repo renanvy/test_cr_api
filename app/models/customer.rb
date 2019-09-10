0970d8673385f3664aaa9b9fbda3f2ac2bdc2566
Renan Carneiro <renanvy@gmail.com>
2019-09-10 14:37:01 -0300

    Add validation to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+
   has_many :carts
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :name }
+  end
+
   context 'relationships' do
     it { is_expected.to have_many :carts }
   end
