da130546e8e90a427a86059175ac2d5addd3fe6a
Renan Carneiro <renanvy@gmail.com>
2019-09-10 14:07:40 -0300

    Add validations to medicine model

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,4 +1,8 @@
 class Medicine < ApplicationRecord
+  validates :name, :value, :quantity, :stock, presence: true
+  validates :value, numericality: { greater_than: 0 }
+  validates :stock, :quantity, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def total
     value * quantity
   end
--- spec/models/medicine_spec.rb
+++ spec/models/medicine_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe Medicine, type: :model do
   let(:valid_attributes) { { name: 'Aspirina', value: 10.0, quantity: 1, stock: 10 } }
   subject { Medicine.new(valid_attributes) }
 
+  context 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :value }
+    it { should validate_presence_of :quantity }
+    it { should validate_presence_of :stock }
+
+    it { should validate_numericality_of(:value).is_greater_than(0) }
+    it { should validate_numericality_of(:stock).only_integer.is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:quantity).only_integer.is_greater_than_or_equal_to(0) }
+  end
+
   context '#total' do
     it 'multiplies' do
       expect(subject.total).to eql 10.0
